a1275e89472349886ec7fc83eb31180b684220bf
crewai_dev <crewai.dev@example.com>
2025-01-09 15:47:21 +0000

    removed Train and Test to try out crew

--- src/oneaskdb/main.py
+++ src/oneaskdb/main.py
@@ -20,39 +20,42 @@ def run():
     }
     Oneaskdb().crew().kickoff(inputs=inputs)
 
+if __name__ == "__main__":
+    run()
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        Oneaskdb().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
+# def train():
+#     """
+#     Train the crew for a given number of iterations.
+#     """
+#     inputs = {
+#         "topic": "AI LLMs"
+#     }
+#     try:
+#         Oneaskdb().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        Oneaskdb().crew().replay(task_id=sys.argv[1])
+#     except Exception as e:
+#         raise Exception(f"An error occurred while training the crew: {e}")
 
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
+# def replay():
+#     """
+#     Replay the crew execution from a specific task.
+#     """
+#     try:
+#         Oneaskdb().crew().replay(task_id=sys.argv[1])
 
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        Oneaskdb().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+#     except Exception as e:
+#         raise Exception(f"An error occurred while replaying the crew: {e}")
 
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
+# def test():
+#     """
+#     Test the crew execution and returns the results.
+#     """
+#     inputs = {
+#         "topic": "AI LLMs"
+#     }
+#     try:
+#         Oneaskdb().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while replaying the crew: {e}")
